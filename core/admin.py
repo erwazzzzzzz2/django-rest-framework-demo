886665cb346dca271e6f7c6bef2ea9c2f7d54e3c
davt <thorne.dav@yahoo.com>
2024-02-05 17:42:00 +0000

    Added code to customize admin note we have a name not username ad so on

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,56 @@
+"""
+Django admin customization
+
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
 
 # Register your models here.
+from core import models
+
+# Add translation. Future proof for I18n )best practice)
+# future proof for  I18n
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (translate("Personal Info"), {"fields": ("name",)}),
+        (
+            translate("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (translate("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(models.User, UserAdmin)  # using custom UserAdmin class
