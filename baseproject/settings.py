d9d0b3f6d6a52364fdb7964232e2b8aac9c98227
davt <thorne.dav@yahoo.com>
2024-02-07 09:19:49 +0000

    Added new frameworks and apps to setting , adjusted sytic paths etc

--- baseproject/settings.py
+++ baseproject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "drf_spectacular",
     "core",
 ]
