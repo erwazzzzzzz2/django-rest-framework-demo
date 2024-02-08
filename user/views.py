631eb4f6a5faa9eeaef04ad599ea1bc9bda7de0b
davt <thorne.dav@yahoo.com>
2024-02-08 15:05:25 +0000

    Added user to views and urls

--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ This applies the correct serializer to the create user POST request
 
 """
 
-from rest_framework import generics
+from rest_framework import authentication, generics, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -28,3 +28,20 @@ class CreateTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+
+    serializer_class = UserSerializer
+    # Auth
+    authentication_classes = [authentication.TokenAuthentication]
+    # Permissions
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        # get authed user
+        # return to serialise
+        # serializer returns serialised data
+        return self.request.user
